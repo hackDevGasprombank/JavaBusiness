a70cd3a428ff4ae4cf9b693245edf1ee3a1d0c5a
DanKoDi <hirofool502@gmail.com>
2025-09-20 15:57:49 +0300

    start tools

---
+++ src/main/java/com/example/gasprombankjavabusiness/JavaBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.example.gasprombankjavabusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBusinessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gasprombankjavabusiness/JavaBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gasprombankjavabusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
